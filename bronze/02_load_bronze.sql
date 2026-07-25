7aaace2ba18aabc64e1e1d7a2508f1af1315c7b6
shaikh riyaz ali <sshaikhriyazali@gmail.com>
2026-07-25 19:49:27 +0530

    Update 02_load_bronze.sql

--- bronze/02_load_bronze.sql
+++ bronze/02_load_bronze.sql
@@ -1,6 +1,20 @@
 /*
 ===============================================================================
-Master Procedure : Load Bronze Layer
+Stored Procedure: Load Bronze Layer
+===============================================================================
+
+Description:
+    Master procedure responsible for loading the Bronze layer.
+
+Actions:
+    1. Generates a Batch ID.
+    2. Executes all Bronze load procedures.
+    3. Measures total execution time.
+    4. Displays execution summary.
+
+Usage:
+    EXEC bronze.load_bronze;
+
 ===============================================================================
 */
 
@@ -14,43 +28,70 @@ BEGIN
     SET NOCOUNT ON;
 
     DECLARE
+        @BatchId UNIQUEIDENTIFIER,
+        @BatchStart DATETIME2(7),
+        @BatchEnd DATETIME2(7);
 
-        @BatchStart DATETIME2(0),
-        @BatchEnd DATETIME2(0);
+    SET @BatchId = NEWID();
 
     BEGIN TRY
 
         SET @BatchStart = SYSDATETIME();
 
-        PRINT '====================================================';
+        PRINT '============================================================';
         PRINT 'STARTING BRONZE LAYER';
-        PRINT '====================================================';
+        PRINT 'Batch ID   : ' + CAST(@BatchId AS NVARCHAR(36));
+        PRINT 'Start Time : ' + CONVERT(VARCHAR(23), @BatchStart, 121);
+        PRINT '============================================================';
 
-        EXEC bronze.load_customers;
+        EXEC bronze.load_customers @BatchId;
 
-        EXEC bronze.load_products;
+        EXEC bronze.load_products @BatchId;
 
-        EXEC bronze.load_stores;
+        EXEC bronze.load_stores @BatchId;
 
-        EXEC bronze.load_sales;
+        EXEC bronze.load_sales @BatchId;
 
         SET @BatchEnd = SYSDATETIME();
 
-        PRINT '====================================================';
+        PRINT '============================================================';
         PRINT 'BRONZE LAYER COMPLETED';
+        PRINT 'Batch ID       : ' + CAST(@BatchId AS NVARCHAR(36));
+        PRINT 'End Time       : ' + CONVERT(VARCHAR(23), @BatchEnd, 121);
         PRINT 'Total Duration : '
-            + CAST(DATEDIFF(SECOND,@BatchStart,@BatchEnd) AS NVARCHAR(20))
-            + ' Seconds';
-        PRINT '====================================================';
+            + CAST(DATEDIFF(MILLISECOND, @BatchStart, @BatchEnd) AS NVARCHAR(20))
+            + ' ms';
+        PRINT 'Status         : SUCCESS';
+        PRINT '============================================================';
 
     END TRY
 
     BEGIN CATCH
 
-        PRINT '====================================================';
+        SET @BatchEnd = SYSDATETIME();
+
+        INSERT INTO etl.error_log
+        (
+            procedure_name,
+            error_number,
+            error_message,
+            error_line,
+            error_state
+        )
+        VALUES
+        (
+            OBJECT_SCHEMA_NAME(@@PROCID) + '.' + OBJECT_NAME(@@PROCID),
+            ERROR_NUMBER(),
+            ERROR_MESSAGE(),
+            ERROR_LINE(),
+            ERROR_STATE()
+        );
+
+        PRINT '============================================================';
         PRINT 'BRONZE LAYER FAILED';
-        PRINT ERROR_MESSAGE();
-        PRINT '====================================================';
+        PRINT 'Batch ID   : ' + CAST(@BatchId AS NVARCHAR(36));
+        PRINT 'Error      : ' + ERROR_MESSAGE();
+        PRINT '============================================================';
 
         THROW;
 
