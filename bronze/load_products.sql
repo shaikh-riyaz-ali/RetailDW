16e4f0f4020aa78a8a61a2fc36d48d8f1a5d316b
shaikh riyaz ali <sshaikhriyazali@gmail.com>
2026-07-25 12:29:32 +0530

    Create load_products.sql

---
+++ bronze/load_products.sql
@@ -0,0 +1,84 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze products
+===============================================================================
+
+Description:
+    Loads the customers_raw.csv file into bronze.products.
+
+Actions:
+    1. Truncates the Bronze table.
+    2. Loads the CSV file using BULK INSERT.
+    3. Displays rows loaded and execution time.
+
+Usage:
+    EXEC bronze.load_products;
+
+===============================================================================
+*/
+
+USE DWH;
+GO
+
+CREATE OR ALTER PROCEDURE bronze.load_products
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    DECLARE
+        @StartTime DATETIME2(0),
+        @EndTime DATETIME2(0),
+        @FilePath NVARCHAR(500),
+        @SqlCommand NVARCHAR(MAX);
+
+    SET @FilePath = N'D:\data pipeline projrct\retail_dwh_project\bronze\';
+
+    BEGIN TRY
+
+        SET @StartTime = SYSDATETIME();
+
+        PRINT '------------------------------------------------------------';
+        PRINT 'Loading bronze.products';
+        PRINT '------------------------------------------------------------';
+
+        TRUNCATE TABLE bronze.products;
+
+        SET @SqlCommand = N'
+        BULK INSERT bronze.products
+        FROM ''' + @FilePath + 'products_raw.csv''
+        WITH
+        (
+            FIRSTROW = 2,
+            FIELDTERMINATOR='','',
+            ROWTERMINATOR=''0x0A'',
+            TABLOCK
+        );';
+
+        EXEC sp_executesql @SqlCommand;
+
+        SET @EndTime = SYSDATETIME();
+
+        PRINT 'Rows Loaded : '
+            + CAST(@@ROWCOUNT AS NVARCHAR(20));
+
+        PRINT 'Duration    : '
+            + CAST(DATEDIFF(SECOND,@StartTime,@EndTime) AS NVARCHAR(20))
+            + ' Seconds';
+
+        PRINT 'Products Loaded Successfully';
+        PRINT '------------------------------------------------------------';
+
+    END TRY
+
+    BEGIN CATCH
+
+        PRINT 'Error Loading bronze.products';
+        PRINT ERROR_MESSAGE();
+
+        THROW;
+
+    END CATCH
+
+END;
+GO
