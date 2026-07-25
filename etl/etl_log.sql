53e79c9e81669380961a4cfb30d9e7aae3569c33
shaikh riyaz ali <sshaikhriyazali@gmail.com>
2026-07-25 19:47:07 +0530

    Update etl_log.sql

--- etl/etl_log.sql
+++ etl/etl_log.sql
@@ -7,23 +7,16 @@ GO
 
 CREATE TABLE etl.etl_log
 (
-    log_id           INT IDENTITY(1,1) PRIMARY KEY,
-
-    process_name     NVARCHAR(100),
-
-    table_name       NVARCHAR(100),
-
-    rows_loaded      INT,
-
-    start_time       DATETIME2,
-
-    end_time         DATETIME2,
-
-    duration_ms      INT,
-
-    status           NVARCHAR(20),
-
-    created_date     DATETIME2
-        DEFAULT GETDATE()
+    log_id          INT IDENTITY(1,1) PRIMARY KEY,
+    batch_id        UNIQUEIDENTIFIER,
+    process_name    NVARCHAR(100),
+    table_name      NVARCHAR(100),
+    source_file     NVARCHAR(300),
+    rows_loaded     INT,
+    start_time      DATETIME2(7),
+    end_time        DATETIME2(7),
+    duration_ms     INT,
+    status          NVARCHAR(20),
+    created_date    DATETIME2(7) DEFAULT SYSDATETIME()
 );
 GO
